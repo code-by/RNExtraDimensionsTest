570a51c8ebac1b51f75f1b04df2e5919fe9db70b
codeBlr <code@tut.by>
2017-03-16 17:16:42 +0300

    init

---
+++ android/app/src/main/java/com/dimensionstest/MainActivity.java
@@ -0,0 +1,16 @@
+package com.dimensionstest;
+
+import com.facebook.react.ReactActivity;
+import ca.jaysoo.extradimensions.ExtraDimensionsPackage;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "DimensionsTest";
+    }
+}
